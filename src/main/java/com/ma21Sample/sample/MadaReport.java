22a29c524396b676b31101757b5af2981b4dd3af
Shiraz Benhaim <shiraz.benhaim12@gmail.com>
2022-01-16 18:16:55 +0200

    class MadaReport: Add a structure of a mada's report

---
+++ src/main/java/com/ma21Sample/sample/MadaReport.java
@@ -0,0 +1,21 @@
+package com.ma21Sample.sample;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MadaReport {
+    private String MDACODE;
+    private String IDNum;
+    private String IDType;
+    private String FirstName;
+    private String LastName;
+    private String City;
+    private String Street;
+    private String BuildingNumber;
+    private String Barcode;
+    private String GetDate;
+    private String TakeDate;
+    private String ResultDate;
+}
