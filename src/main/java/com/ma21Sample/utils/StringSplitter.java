a76ca1bc265b076f7fd83f2a27ac7e69cea6e14c
Shiraz Benhaim <shiraz.benhaim12@gmail.com>
2022-01-16 19:28:04 +0200

    class StringSplitter: Create a class that splits csv content to list of arrays

---
+++ src/main/java/com/ma21Sample/utils/StringSplitter.java
@@ -0,0 +1,27 @@
+package com.ma21Sample.utils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class StringSplitter {
+    private final String csvRowSeparator = "\n";
+    private final String csvFieldsSeparator = "\",\"";
+
+    /*
+    * Splits a string that written in csv format, to list of items
+    * content - the string in csv format
+    * returns list of string arrays, that each of them is an item
+    * */
+    public List<String[]> splitStringInCsvFormat(String content) {
+        List<String[]> result = new ArrayList<>();
+
+        String[] lines = content.split(this.csvRowSeparator);
+
+        Arrays.asList(lines).subList(1, lines.length - 1)
+                .forEach(line -> result.add(line.substring(1, line.length() - 1)
+                        .split(csvFieldsSeparator)));
+
+        return result;
+    }
+}
