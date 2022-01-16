98bb34abd3c253638ce5569c15bf022e1318ab61
Shiraz Benhaim <shiraz.benhaim12@gmail.com>
2022-01-16 19:47:25 +0200

    Parse the csv content to lists

--- src/main/java/com/ma21Sample/dataFile/csv/CSVFileParser.java
+++ src/main/java/com/ma21Sample/dataFile/csv/CSVFileParser.java
@@ -1,7 +1,10 @@
 package com.ma21Sample.dataFile.csv;
 
 import com.ma21Sample.dataFile.DataFileParser;
+import com.ma21Sample.fileHandler.ReadFileContent;
+import com.ma21Sample.utils.StringSplitter;
 
+import java.io.IOException;
 import java.util.List;
 
 public class CSVFileParser extends DataFileParser {
@@ -11,6 +14,17 @@ public class CSVFileParser extends DataFileParser {
 
     @Override
     public List<String[]> parseDataFileToLists() {
-        return null;
+        List<String[]> data = null;
+        StringSplitter stringSplitter = new StringSplitter();
+
+        try {
+            ReadFileContent readFileContent = new ReadFileContent(super.srcFile);
+            data = stringSplitter.splitStringInCsvFormat(readFileContent.readContent());
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        return data;
     }
 }
