6338a3a7e3c8bb100aa10b26e3e85485ded11eeb
Shiraz Benhaim <shiraz.benhaim12@gmail.com>
2022-01-16 19:10:48 +0200

    Created class DataFileParser which parse a data file to java object

---
+++ src/main/java/com/ma21Sample/dataFile/DataFileParser.java
@@ -0,0 +1,14 @@
+package com.ma21Sample.dataFile;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+public abstract class DataFileParser {
+    protected String srcFile;
+
+    public abstract List<String[]> parseDataFileToLists();
+}
