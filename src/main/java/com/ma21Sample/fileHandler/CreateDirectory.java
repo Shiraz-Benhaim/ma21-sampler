20b717e880fd83be09fbda1f6b5ee77295634245
Shiraz Benhaim <shiraz.benhaim12@gmail.com>
2022-01-17 09:01:50 +0200

    Create class CreateDirectory to create the reports folder

---
+++ src/main/java/com/ma21Sample/fileHandler/CreateDirectory.java
@@ -0,0 +1,23 @@
+package com.ma21Sample.fileHandler;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.io.File;
+
+@Data
+@AllArgsConstructor
+public class CreateDirectory {
+    private String dirName;
+
+    /*
+    * Function creates a directory if it does not exist
+    * */
+    public void createDirIfNotExisting() {
+        File dir = new File(this.dirName);
+
+        if (!dir.exists()) {
+            dir.mkdir();
+        }
+    }
+}
