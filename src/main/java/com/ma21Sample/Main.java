3de72c8778f18c22cc17e42477b9040e5d0818ab
Shiraz Benhaim <shiraz.benhaim12@gmail.com>
2022-01-17 08:08:30 +0200

    Add convertor from lists to MadaReportsManager

---
+++ src/main/java/com/ma21Sample/Main.java
@@ -0,0 +1,22 @@
+package com.ma21Sample;
+
+import com.ma21Sample.dataFile.DataFileParser;
+import com.ma21Sample.dataFile.csv.CSVFileParser;
+import com.ma21Sample.madaReport.MadaReport;
+import com.ma21Sample.madaReport.MadaReportsManager;
+import com.ma21Sample.madaReport.MadaReportsManagerUtils;
+
+public class Main {
+    public static final String csvPath = "src\\main\\resources\\MadaReports.csv";
+
+    public static void main(String[] args) {
+        MadaReportsManagerUtils managerUtils = new MadaReportsManagerUtils();
+        DataFileParser csvFileParser = new CSVFileParser(csvPath);
+        MadaReportsManager madaReportsManager = managerUtils.
+                listsToMadaReportsManager(csvFileParser.parseDataFileToLists());
+
+        for (MadaReport report : madaReportsManager.getReports()) {
+            System.out.println(report.getFirstName() + " " + report.getLastName());
+        }
+    }
+}
---
+++ src/main/java/com/ma21Sample/madaReport/MadaReportsManagerUtils.java
@@ -0,0 +1,38 @@
+package com.ma21Sample.madaReport;
+
+import java.util.List;
+
+public class MadaReportsManagerUtils {
+    /*
+     * Function convert a list of strings with the information about the
+     *  reports to a list of reports (MadaReportsManager)
+     * reportsData - the list with the information about the reports
+     * returns a list of reports with ths information
+     * */
+    public MadaReportsManager listsToMadaReportsManager (List<String[]> reportsData) {
+        MadaReportsManager reportsManager = new MadaReportsManager();
+
+        try {
+            for (String[] record : reportsData) {
+                reportsManager.addMadaReport(new MadaReport(
+                    record[MadaReportFieldsIndex.MDA_CODE.getIndex()],
+                    Integer.parseInt(record[MadaReportFieldsIndex.ID_NUM.getIndex()]),
+                    Integer.parseInt(record[MadaReportFieldsIndex.ID_TYPE.getIndex()]),
+                    record[MadaReportFieldsIndex.FIRST_NAME.getIndex()],
+                    record[MadaReportFieldsIndex.LAST_NAME.getIndex()],
+                    record[MadaReportFieldsIndex.CITY.getIndex()],
+                    record[MadaReportFieldsIndex.STREET.getIndex()],
+                    Integer.parseInt(record[MadaReportFieldsIndex.BUILDING_NUMBER.getIndex()]),
+                    record[MadaReportFieldsIndex.BAR_CODE.getIndex()],
+                    record[MadaReportFieldsIndex.GET_DATE.getIndex()],
+                    record[MadaReportFieldsIndex.TAKE_DATE.getIndex()],
+                    record[MadaReportFieldsIndex.RESULT_DATE.getIndex()]
+                ));
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        return reportsManager;
+    }
+}
