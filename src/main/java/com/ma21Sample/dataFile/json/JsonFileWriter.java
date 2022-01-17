10a591f70ae9a895b8392ba8d6b07c44a936416b
Shiraz Benhaim <shiraz.benhaim12@gmail.com>
2022-01-17 09:04:27 +0200

    Write the MadaReportsManager to a json file

--- src/main/java/com/ma21Sample/dataFile/json/JsonFileWriter.java
+++ src/main/java/com/ma21Sample/dataFile/json/JsonFileWriter.java
@@ -1,16 +1,36 @@
 package com.ma21Sample.dataFile.json;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ma21Sample.dataFile.DataFileWriter;
+import com.ma21Sample.madaReport.MadaReportsManager;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 public class JsonFileWriter extends DataFileWriter {
-    public JsonFileWriter(String dstFile) {
+    private MadaReportsManager reportsManager;
+
+    public JsonFileWriter(String dstFile, MadaReportsManager madaReportsManager) {
         super(dstFile);
+        this.reportsManager = madaReportsManager;
+    }
+
+    public MadaReportsManager getMadaReportsManager() {
+        return reportsManager;
+    }
+
+    public void setMadaReportsManager(MadaReportsManager reportsManager) {
+        this.reportsManager = reportsManager;
     }
 
     @Override
-    public List<String[]> writeObjectToDataFile() {
-        return null;
+    public void writeObjectToDataFile() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            objectMapper.writeValue(new File(super.dstFile), this.reportsManager);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
