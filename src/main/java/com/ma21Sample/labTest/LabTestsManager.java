08665fddfaa93833be77a84bd9ed3b723b60eebc
Shiraz Benhaim <shiraz.benhaim12@gmail.com>
2022-01-18 08:53:57 +0200

    Add local names to the attributes for the xml headers

--- src/main/java/com/ma21Sample/labTest/LabTestsManager.java
+++ src/main/java/com/ma21Sample/labTest/LabTestsManager.java
@@ -1,5 +1,8 @@
 package com.ma21Sample.labTest;
 
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -8,7 +11,11 @@ import java.util.List;
 
 @Data
 @AllArgsConstructor
+@JacksonXmlRootElement(localName = "labTests")
 public class LabTestsManager {
+
+    @JacksonXmlElementWrapper(useWrapping = false)
+    @JacksonXmlProperty(localName = "labTest")
     private List<LabTest> results;
 
     public LabTestsManager() {
